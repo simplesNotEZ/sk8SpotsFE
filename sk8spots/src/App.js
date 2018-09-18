087e3630ae0c610ec6400739421d663e55c2b32c
Benjamin Austin <bnjmn80@gmail.com>
2018-09-18 13:24:36 -0600

    Got React-Router working.  Styled up card list-item for various skate spots.

--- sk8spots/src/App.js
+++ sk8spots/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom' 
-import sk8RDie from './skate-or-die.png';
-import churchPipe from './churchPipe.jpg';
-import oldSpot from './blackNWhiteSpot.jpeg';
 import './App.css';
 
 import Header from './components/Header'
+import Home from './components/Home'
+import Someday from './components/Someday'
+import Yesterday from './components/Yesterday'
 import Footer from './components/Footer'
 
 class App extends Component {
@@ -16,32 +16,15 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <Header title={this.state.title[0]} />
-        <p className="App-intro">
-          Where you gonna skate today?
-        </p>
-        <div className="allSpots">
-          <Link to="/someday">
-            <section className="wishSpots mainSpotsCard">
-              <h2>Someday Spots</h2>
-              <img src={churchPipe} className="mainSpotsImage" alt="a halfpipe in a church"/>
-            </section>
-          </Link>
-          <Link to="/yesterday">
-            <section className="theOtherDaySpots mainSpotsCard">
-              <h2>Yesterday Spots</h2>
-              <img src={oldSpot} className="mainSpotsImage" alt="old-school skate spot"/>
-            </section>
-          </Link>
+        <div className="App">
+          <Header title={this.state.title[0]} />
+          <div className="content">
+            <Route exact path="/" component={Home} /> 
+            <Route path="/someday" component={Someday} />
+            <Route path="/yesterday" component={Yesterday} /> 
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-
-      {/* <Route exact path="/" component={Home} />
-      <Route path="/someday" component={Someday} />
-      <Route path="/yesterday" component={Yesterday} /> */}
-
       </Router>
     );
   }
---
+++ sk8spots/src/components/Yesterday.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+import Header from './Header'
+import Footer from './Footer'
+
+const Yesterday = (props) => {
+    // console.log(props);
+    
+    return (
+        <div className="somedayDiv">
+            <h1>Holy Shit Ladies!</h1>
+            <h2>What'chu got on my forty now?</h2>
+        </div>
+    )
+}
+
+export default Yesterday;
